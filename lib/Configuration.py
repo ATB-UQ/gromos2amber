045f1df3047bc8b34470fee6cc6746ee597d7331
Tom Lee <contact@tomlee.com.au>
2016-09-28 10:53:13 +1000

    Convert configuration coordinates to angstroms

--- lib/Configuration.py
+++ lib/Configuration.py
@@ -1,13 +1,15 @@
 import gromos_format as gf
 
-class Configuration:
+NANOMETRE = 10.0
 
+class Configuration:
     def __init__(self, io):
         blocks = gf.parse_blocks(io)
         block = blocks["POSITION"]
+        nm = NANOMETRE
         _,_,_,_,x,y,z = gf.parse_simple_columns(block, [5,6,6,7,15,15,15],
                                             (int,str,str,int,float,float,float),
                                             header = False)
-        self.positions = [ [xi,yi,zi] for xi,yi,zi in zip(x,y,z) ]
+        self.positions = [ [xi*nm,yi*nm,zi*nm] for xi,yi,zi in zip(x,y,z) ]
 
 
